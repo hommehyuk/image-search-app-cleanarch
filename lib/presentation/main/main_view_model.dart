893b34e74350265e533419b36b309d783d66cb36
hommehyuk <bin6827@gmail.com>
2024-01-10 16:58:03 +0900

    feat: use case 구현 및 적용

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app_cleanarch/domain/use_case/search_all_image_with_cache_use_case.dart';
 
 import '../../domain/model/image_item.dart';
 
 class MainViewModel extends ChangeNotifier {
+  final _searchNormalUseCase = SearchAllImageWithCacheUseCase();
+
   List<ImageItem> imageItems = [];
   bool isLoading = false;
 
@@ -10,6 +13,7 @@ class MainViewModel extends ChangeNotifier {
     isLoading = true;
     notifyListeners();
 
+    imageItems = await _searchNormalUseCase.execute(query);
     isLoading = false;
     notifyListeners();
   }
